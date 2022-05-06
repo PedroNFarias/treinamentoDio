5abce332aabaf43576f1a5cd19713ed6152830fd
PedroNFarias <pedrosilvafarias@gmail.com>
2022-05-05 23:21:59 -0300

    Bootstrap adicionado

---
+++ paginaWebSimples/tests/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Meu primeiro site em PHP</title>
+    </head>
+    <body>
+        <?php
+            for ( $i = 0; $i < 10; $i++ ){
+                print("Linha número " . $i . "<br />");
+            }
+        ?>
+    </body>
+    
+</html>
